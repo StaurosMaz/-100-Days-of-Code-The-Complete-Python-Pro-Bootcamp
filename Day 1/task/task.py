916b7c452b959bcdeeecbffb600d3674770bc317
StaurosMaz <smazanakis698@gmail.com>
2026-04-29 10:40:24 +0300

    added task 5/6

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,4 @@
-name = "Angela"
-length = len(name)
-print(length)
+print("Welcome to the Band Name Generator.")
+city = input("What city did you grow up in?\n")
+petName = input("What is your pet name?\n")
+print("Your Band Name is "+city+" "+petName)
